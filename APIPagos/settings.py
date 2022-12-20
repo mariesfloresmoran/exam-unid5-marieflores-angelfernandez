485d1239c0545bb412852ea3ce373d361cae2e1d
Jokemura <angelfv1235@gmail.com>
2022-12-20 16:34:05 -0500

    Config Database for railway

--- APIPagos/settings.py
+++ APIPagos/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'APIPagos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db2.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER' : 'root',
+        'PASSWORD': 'VUGHBfCieffzHfVMLUdK',
+        'HOST' : 'containers-us-west-140.railway.app',
+        'PORT' : '6833',
     }
 }
 
